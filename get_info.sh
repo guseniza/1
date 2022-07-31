084729799a9d5a0d26a8dec1660decbdbf92ff5b
MaksymChu <guseniza@gmail.com>
2022-07-31 18:33:08 +0300

    add org_name

--- get_info.sh
+++ get_info.sh
@@ -4,7 +4,7 @@ TMP_FILE=`mktemp`
 
 list_as='as_fck.src'
 
-echo "aut-num;org;descr"
+echo "aut-num;org;org-name;descr"
 echo "================="
 
 while read as_obj
@@ -12,8 +12,9 @@ do
 
 	whois "$as_obj"  > "$TMP_FILE"
 	descr=`cat "$TMP_FILE" | grep -A 5 'aut-num'|  awk -F ':' '{ if ($1 == "descr") {print $2} }' | head -n 1 | sed 's/^ *//g'`
+	org_name=`cat "$TMP_FILE" | grep 'org-name' | awk -F ':' '{print $2}' | sed 's/^ *//g' `
 	org=`cat "$TMP_FILE" | awk -F ':' '{ if ($1 == "org") {print $2} }' | sed 's/^ *//g'`
 
-	echo "$as_obj;$org;$descr"
+	echo "$as_obj;$org;$org_name;$descr"
 
 done < $list_as
